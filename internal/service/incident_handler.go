b85d2cb1eb0fed562c1cd09b4612104505fb4d3c
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2022-06-16 17:31:32 +0100

    Implement 'GetYearStats' method for incident_handler

--- internal/service/incident_handler.go
+++ internal/service/incident_handler.go
@@ -11,6 +11,17 @@ import (
 	"github.com/lucassouzavieira/go-grpc-server/pkg/protobuf/incident"
 )
 
+type GroupStats struct {
+	animal_group string
+	incidents    int32
+}
+
+type Stats struct {
+	year      int32
+	incidents int32
+	groups    []*GroupStats
+}
+
 type IncidentHandler struct {
 	r *repository.Repository
 	i []*incident.Incident
@@ -40,6 +51,24 @@ func NewIncidentHandler(r *repository.Repository) (*IncidentHandler, error) {
 	}, nil
 }
 
+func (h *IncidentHandler) Filter(fs []*repository.Filter) (bool, error) {
+	data, err := h.r.Filter(fs)
+
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	var ixs []*incident.Incident
+	for _, v := range data {
+		ix := incidentFromCsv(v)
+		ixs = append(ixs, &ix)
+	}
+
+	h.i = ixs
+	return true, nil
+}
+
 func (h *IncidentHandler) GetIncidents() ([]*incident.Incident, error) {
 	return h.i, nil
 }
@@ -54,6 +83,49 @@ func (h *IncidentHandler) GetIncidentsByAnimalGroup(group string) ([]*incident.I
 	}
 
 	return filtered, nil
+}
+
+func (h *IncidentHandler) GetYearStats(year int32) (*Stats, error) {
+	var fx = repository.Filter{
+		Property: "CalYear",
+		Value:    decimal.NewFromInt32(year).StringFixed(0),
+	}
+
+	var fs = make([]*repository.Filter, 0)
+	fs = append(fs, &fx)
+
+	_, err := h.Filter(fs)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": "Error trying to filter incidents",
+		}).Warn(err)
+
+		return nil, err
+	}
+
+	groups := make(map[string]int32)
+
+	for _, i := range h.i {
+		groups[i.GetAnimalGroup()] = groups[i.GetAnimalGroup()] + 1
+	}
+
+	group_stats := make([]*GroupStats, 0)
+
+	for g, i := range groups {
+		gs := GroupStats{
+			animal_group: g,
+			incidents:    i,
+		}
+
+		group_stats = append(group_stats, &gs)
+	}
+
+	return &Stats{
+		year:      year,
+		incidents: int32(len(h.i)),
+		groups:    group_stats,
+	}, nil
 }
 
 // Internal
